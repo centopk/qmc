af748aa09fa827c69fb85e8c3e934cb2b0c8b5cd
centopk <90838713+centopk@users.noreply.github.com>
2026-05-18 21:38:30 +0300

    Update __init__.py

--- components/my_qmc5883l/__init__.py
+++ components/my_qmc5883l/__init__.py
@@ -3,8 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import i2c, sensor
 from esphome.const import (
     CONF_ID,
-    CONF_ADDRESS,
-    CONF_UPDATE_INTERVAL,
+    CONF_NAME,
     DEVICE_CLASS_MAGNETIC_FIELD,
     STATE_CLASS_MEASUREMENT,
     UNIT_MICROTESLA,
@@ -22,30 +21,20 @@ CONF_X = "field_strength_x"
 CONF_Y = "field_strength_y"
 CONF_Z = "field_strength_z"
 
+_SENSOR_SCHEMA = sensor.sensor_schema(
+    unit_of_measurement=UNIT_MICROTESLA,
+    accuracy_decimals=1,
+    device_class=DEVICE_CLASS_MAGNETIC_FIELD,
+    state_class=STATE_CLASS_MEASUREMENT,
+)
+
 CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(MyQMC5883LComponent),
-            cv.Optional(CONF_X): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROTESLA,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_MAGNETIC_FIELD,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_Y): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROTESLA,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_MAGNETIC_FIELD,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-            cv.Optional(CONF_Z): sensor.sensor_schema(
-                unit_of_measurement=UNIT_MICROTESLA,
-                accuracy_decimals=1,
-                device_class=DEVICE_CLASS_MAGNETIC_FIELD,
-                state_class=STATE_CLASS_MEASUREMENT,
-            ),
-        }
-    )
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(MyQMC5883LComponent),
+        cv.Optional(CONF_X, default={CONF_NAME: "Mag X"}): _SENSOR_SCHEMA,
+        cv.Optional(CONF_Y, default={CONF_NAME: "Mag Y"}): _SENSOR_SCHEMA,
+        cv.Optional(CONF_Z, default={CONF_NAME: "Mag Z"}): _SENSOR_SCHEMA,
+    })
     .extend(cv.polling_component_schema("100ms"))
     .extend(i2c.i2c_device_schema(0x0D))
 )
@@ -56,12 +45,9 @@ async def to_code(config):
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
 
-    if CONF_X in config:
-        sens = await sensor.new_sensor(config[CONF_X])
-        cg.add(var.set_x_sensor(sens))
-    if CONF_Y in config:
-        sens = await sensor.new_sensor(config[CONF_Y])
-        cg.add(var.set_y_sensor(sens))
-    if CONF_Z in config:
-        sens = await sensor.new_sensor(config[CONF_Z])
-        cg.add(var.set_z_sensor(sens))
+    sens = await sensor.new_sensor(config[CONF_X])
+    cg.add(var.set_x_sensor(sens))
+    sens = await sensor.new_sensor(config[CONF_Y])
+    cg.add(var.set_y_sensor(sens))
+    sens = await sensor.new_sensor(config[CONF_Z])
+    cg.add(var.set_z_sensor(sens))
